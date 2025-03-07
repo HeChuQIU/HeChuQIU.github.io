8798b90c26a22ada9fda65a6c9e99eca52e9e1e2
HeChu <2296282722@qq.com>
2025-03-07 21:56:27 +0800

    20250307

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,24 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import expressiveCode from "astro-expressive-code";
+import {pluginLineNumbers} from "@expressive-code/plugin-line-numbers";
+
+import '@fontsource-variable/jetbrains-mono';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://HeChuQIU.github.io',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		expressiveCode({
+			plugins: [pluginLineNumbers()], // add pluginLineNumbers() plugin
+			defaultProps: {
+				showLineNumbers: true, // set true to show line number by default
+			}
+		}),
+		mdx(), sitemap(),
+	],
 	i18n: {
 		defaultLocale: "zh-cn",
 		locales: ["zh-cn", "en"],
